a56cf6ed08614fda37c881e70435072bcde709e6
Vadim Kudriavtsev <x.kydrik.x@gmail.com>
2021-01-25 17:14:32 +0300

    step 3: add simple json diff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+import path from 'path';
+
 import genDiff from '../index.js';
 
 import commander from 'commander';
 const { Command } = commander;
+
+function readJsonFile(filepath) {
+  const resolvedPath = path.resolve(filepath);
+  const fileContent = fs.readFileSync(resolvedPath);
+
+  return JSON.parse(fileContent);
+}
 
 function parseCommandLine() {
   const program = new Command();
@@ -14,7 +24,15 @@ function parseCommandLine() {
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
     .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .action((filepath1, filepath2) => {
+      const json1 = readJsonFile(filepath1);
+      const json2 = readJsonFile(filepath2);
+
+      const diff = genDiff(json1, json2);
+
+      console.log(diff);
+    });
 
   program.parse();
 }
--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+import _ from 'lodash';
 
-export default function genDiff() {
+export default function genDiff(jsonA, jsonB) {
+  const keys = _.union(Object.keys(jsonA), Object.keys(jsonB)).sort();
 
+  const diffs = keys.map(key => {
+    const hasA = jsonA.hasOwnProperty(key);
+    const hasB = jsonB.hasOwnProperty(key);
+
+    const valueA = jsonA[key];
+    const valueB = jsonB[key];
+
+    if (valueA === valueB) {
+      return `   ${key}: ${valueA}`;
+    }
+
+    if (hasA && !hasB) {
+      return ` - ${key}: ${valueA}`;
+    }
+
+    if (!hasA && hasB) {
+      return ` + ${key}: ${valueB}`;
+    }
+
+    return ` - ${key}: ${valueA}\n + ${key}: ${valueB}`
+  });
+
+  const diffResult = diffs.join('\n');
+
+  return `{\n${diffResult}\n}`;
 }
